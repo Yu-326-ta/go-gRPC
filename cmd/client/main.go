8d863669fd1be0edc01c6479ff4ede1a6851010c
Yuta Yoshinaga <yoshinaga-yuta@dmm.com>
2023-10-03 17:22:19 +0900

    implement server streaming

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -4,7 +4,9 @@ import (
 	// (一部抜粋)
 	"bufio"
 	"context"
+	"errors"
 	"fmt"
+	"io"
 	"log"
 	"os"
 
@@ -44,7 +46,8 @@ func main() {
 
 	for {
 		fmt.Println("1: send Request")
-		fmt.Println("2: exit")
+		fmt.Println("2: HelloserverStream")
+		fmt.Println("3: exit")
 		fmt.Print("please enter >")
 
 		scanner.Scan()
@@ -53,8 +56,9 @@ func main() {
 		switch in {
 		case "1":
 			Hello()
-
 		case "2":
+			HelloServerStream()
+		case "3":
 			fmt.Println("bye.")
 			goto M
 		}
@@ -75,5 +79,33 @@ func Hello() {
 		fmt.Println(err)
 	} else {
 		fmt.Println(res.GetMessage())
+	}
+}
+
+func HelloServerStream() {
+	fmt.Println("Please enter your name.")
+	scanner.Scan()
+	name := scanner.Text()
+
+	req := &hellopb.HelloRequest{
+		Name: name,
+	}
+	stream, err := client.HelloServerStream(context.Background(), req)
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	for {
+		res, err := stream.Recv()
+		if errors.Is(err, io.EOF) {
+			fmt.Println("all the responses have already received.")
+			break
+		}
+
+		if err != nil {
+			fmt.Println(err)
+		}
+		fmt.Println(res)
 	}
 }
